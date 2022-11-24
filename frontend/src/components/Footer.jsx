e104b4b5f657c11321cc5fc0b4d5b60e039fceb1
GonçaloDiogoCoelho <goncalodmvm.c@gmail.com>
2022-11-24 17:09:08 +0000

    footer styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,10 +4,14 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer-all">
-      <div className="div-1">
-        <h2>This is the result of a Christmas Hackathon at Wild Code School</h2>
+      <div className="footer-div-1">
+        <h2>
+          This is the result of a Christmas Hackathon
+          <br />
+          at Wild Code School
+        </h2>
       </div>
-      <div className="div-2">
+      <div className="footer-div-2">
         <div className="links">
           <a href="https://twitter.com/" target="_blank" rel="noreferrer">
             Twitter
@@ -20,7 +24,7 @@ function Footer() {
           </a>
         </div>
       </div>
-      <div className="div-3">
+      <div className="footer-div-3">
         <p id="copyrights">&copy; 2022 Christmas Crew</p>
       </div>
     </div>
