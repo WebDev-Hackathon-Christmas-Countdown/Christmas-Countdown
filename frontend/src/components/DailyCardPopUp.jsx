bdcd8595d63ced383e9ea706efc374ac578cc712
GonçaloDiogoCoelho <goncalodmvm.c@gmail.com>
2022-11-25 10:05:24 +0000

    button to show answer

--- frontend/src/components/DailyCardPopUp.jsx
+++ frontend/src/components/DailyCardPopUp.jsx
@@ -4,10 +4,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/button-has-type */
-import React from "react";
+import React, { useState } from "react";
 import "./DailyCardPopUp.css";
 
 function DailyCardPopUp(props) {
+  const [showAnswerClass, setShowAnswerClass] = useState(false);
+  const showAnswer = () => {
+    setShowAnswerClass(!showAnswerClass);
+  };
+
   const { window, handleClose } = props;
   return (
     <div className="popup-box" onClick={handleClose}>
@@ -15,7 +20,15 @@ function DailyCardPopUp(props) {
         <img src={window.gif} alt="It's Christmas!" className="card-gif" />
         <div className="popup-card-title"> It's Christmas!!! </div>
         <div className="card-question">{window.question}</div>
-        <div className="card-answer">{window.answer}</div>
+        <div className={showAnswerClass ? "card-answer" : "hide-card-answer"}>
+          {window.answer}
+        </div>
+        <input
+          type="button"
+          className={showAnswerClass ? "hide-answer-button" : "answer-button"}
+          value="Know the answer!"
+          onClick={showAnswer}
+        />
         <iframe
           width="400"
           height="80"
